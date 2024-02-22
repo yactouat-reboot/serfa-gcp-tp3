05f9e6de33dec44f63a7c2b7a78eb095a47cea10
yactouat <yactouat@hotmail.com>
2024-02-22 09:56:01 +0100

    TODO php router

--- html/index.php
+++ html/index.php
@@ -1,40 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Meta -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PerfectTicket | your tickets</title>
+<?php
 
-    <!-- Favicon part -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
-    <h1>Perfect Ticket | your tickets</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Ticket ID</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Status</th>
-                <th>Assigned To</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-
-    <script src="./javascript/frontend-bundle.js"></script>
-</body>
-</html>
+// get the requested path
+$path = $_SERVER['REQUEST_URI'];
+die($path);
